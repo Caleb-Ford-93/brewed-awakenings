845323bba893b23a5741612854f992af10a2cac2
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-11 10:33:26 -0500

    added product click event

--- scripts/Products.js
+++ scripts/Products.js
@@ -6,11 +6,26 @@ export const Products = () => {
     let html = `<ul>`
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product"
+                     data-name="${product.name}"
+                     data-price="${product.price}">
+                 ${product.name}</li>`
     }
 
     html += "</ul>"
 
     return html
 }
+
+document.addEventListener (
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if(itemClicked.dataset.type === "product") {
+            const priceInt = Math.round(Number(itemClicked.dataset.price) * 100)
+            const displayPrice = (priceInt / 100).toFixed(2)
+            window.alert(`${itemClicked.dataset.name} costs\n$${displayPrice}`)
+        }
+    }
+)
 
